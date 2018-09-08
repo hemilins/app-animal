d2a4b4fb4b5c44ecf0b90492a564629a6916a6f5
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2018-09-08 19:51:02 -0300

    Adicionando tela de registrar, máscaras em campos e ajuste tela login

--- app/src/main/java/br/ufsc/tic/appanimal/LoginActivity.java
+++ app/src/main/java/br/ufsc/tic/appanimal/LoginActivity.java
@@ -1,7 +1,10 @@
 package br.ufsc.tic.appanimal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView registrar = (TextView) findViewById(R.id.acao_registrar);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(LoginActivity.this, RegistrarActivity.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
