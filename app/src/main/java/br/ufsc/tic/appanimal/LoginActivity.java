e70bab575fa7b0ead22af90ae76c7b665fea458e
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2018-09-09 15:25:31 -0300

    Adicionando tela principal

--- app/src/main/java/br/ufsc/tic/appanimal/LoginActivity.java
+++ app/src/main/java/br/ufsc/tic/appanimal/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,10 +15,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         TextView registrar = (TextView) findViewById(R.id.acao_registrar);
-
         registrar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(LoginActivity.this, RegistrarActivity.class);
+                startActivity(it);
+            }
+        });
+
+        Button logar = (Button) findViewById(R.id.button_entrar);
+        logar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(it);
             }
         });
